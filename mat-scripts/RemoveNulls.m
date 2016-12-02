3b5f34438e79d72a0b2ff63e1c3eb0b201523dd6
Shivam Sharma <sdsharma@ucsd.edu>
2016-12-01 16:50:56 -0800

    Update RemoveNulls.m

--- mat-scripts/RemoveNulls.m
+++ mat-scripts/RemoveNulls.m
@@ -1,20 +1,14 @@
-function [result] = RemoveNulls(dataset_name)
+function return_mat = RemoveNulls(input)
+    result_cells = struct2cell(input(1,1));
+    return_mat = [];
 
-result = load('../mat-data/' + dataset_name);
-result_cells = struct2cell(result(1,1));
-return_mat = [];
-
-% TODO: parse and remove nulls
-
-for i=1:length(result_cells)
-    try
-        cell = cell2mat(result_cells(i));
-        return_mat = [return_mat; cell];
-    catch
-        result_cells{i,1}{3} = 0;
-        cell = cell2mat(result_cells{i});
+    for i=1:length(result_cells)
+        try
+            cell = cell2mat(result_cells(i));
+        catch
+            result_cells{i,1}{3} = 0;
+            cell = cell2mat(result_cells{i});
+        end
         return_mat = [return_mat; cell];
     end
-end
-result = return_mat;
 end
