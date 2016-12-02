06d93cdba3eff1f79063e5b6eb5e1e9181d15f88
Shivam Sharma <sdsharma@ucsd.edu>
2016-12-01 23:02:06 -0800

    Add surface plot using visjs

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 new WOW().init();
-var data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+var data = new vis.DataSet();
+var counter = 0;
+var steps = 50;  // number of datapoints will be steps*steps
+var axisMax = 314;
+var axisStep = axisMax / steps;
+for (var x = 0; x < axisMax; x+=axisStep) {
+    for (var y = 0; y < axisMax; y+=axisStep) {
+        var value = (3.688 + (-0.00003891*x) + (0.0003306*y));
+        data.add({id:counter++,x:x,y:y,z:value,style:value});
+    }
+}
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+// specify options
+var options = {
+    width:  '100%',
+    height: '550px',
+    style: 'surface',
+    showPerspective: true,
+    showGrid: true,
+    showShadow: false,
+    keepAspectRatio: true,
+    verticalRatio: 0.7
+};
 
+// Instantiate our graph object.
+var container = document.getElementById('chart');
+var graph3d = new vis.Graph3d(container, data, options);
 hljs.initHighlightingOnLoad();
